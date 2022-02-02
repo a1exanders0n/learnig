95cd015d132c95b8fe2b5cbb53181108ca3b38a2
Max <alxndrsn.dsgn@gmail.com>
2022-02-02 16:50:54 +0200

    tttt

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //learning
+function showMessage() {
+    let message = "Привіт, я JavaScript!"; // локальна змінна
+  
+    alert( message );
+  }
+  
+  showMessage(); // Привіт, я JavaScript!
+  
